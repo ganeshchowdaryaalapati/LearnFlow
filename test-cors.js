8eb9ce2efe9ba81843a6373c88e45e39be1f6804
Ganesh Chowdary  Aalapati <222621417+ganeshchowdaryaalapati@users.noreply.github.com>
2026-07-21 12:02:38 +0530

    Delete test-cors.js

--- test-cors.js
+++
@@ -1,6 +0,0 @@
-fetch('https://extendsclass.com/api/json-storage/bin/eeeddfe')
-  .then(res => {
-    console.log('CORS headers:');
-    res.headers.forEach((val, key) => console.log(key, val));
-  })
-  .catch(console.error);
